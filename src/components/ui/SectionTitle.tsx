7101f7608a301f202033a488e44b75e47b4de8cd
Amir Ekbatanifard <amiref007@gmail.com>
2026-05-26 23:50:08 +0330

    feat: create SectionTitle component

---
+++ src/components/ui/SectionTitle.tsx
@@ -0,0 +1,11 @@
+interface Props {
+    text: string;
+}
+
+export default function SectionTitle({ text}: Props) {
+    return (
+        <p className="text-title">
+            {text}
+        </p>
+    );
+}
