6cd65a4587b64317674cfd01d1cd1b6e1bdc3902
Amir Ekbatanifard <amiref007@gmail.com>
2026-05-26 13:42:36 +0330

    feat: create new component Input.tsx

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,19 @@
+import { twMerge } from "tailwind-merge";
+
+interface Props {
+    placeholder?: string;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    style?: string;
+}
+
+export default function Input({ onChange, placeholder, style, value }: Props) {
+    return (
+        <input
+            className={twMerge("search-input", style)}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+        />
+    );
+}
