e54575f4b798c6fe0779265a56f52c8d1e0ad9f1
Amir Ekbatanifard <amiref007@gmail.com>
2026-05-27 19:31:04 +0330

    style: move tailwind CSS styling to component Badge.tsx, plus improving styles

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -3,12 +3,24 @@ import { twMerge } from "tailwind-merge"
 interface Props {
     style?: string;
     text?: string;
+    type: 'success' | 'error' | 'warning' | 'info';
 }
 
-export default function Badge({ text, style }: Props) {
+export default function Badge({ text, type, style }: Props) {
     return (
-        <div className={twMerge("badge", style)}>
+        <div className={twMerge(`
+            inline-flex items-center justify-center py-1
+            px-2.5 rounded-[999px] text-xs font-medium
+            ${variants[type]}
+        `, style)}>
             {text}
         </div>
     );
 }
+
+const variants = {
+    success: 'bg-success/10 text-success',
+    error: 'bg-error/10 text-error',
+    warning: 'bg-warning/10 text-warning',
+    info: 'bg-info/10 text-info',
+};
