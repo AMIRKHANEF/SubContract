39d83b94b47e9b98fe117bf0547c9ce997fcb83e
Amir Ekbatanifard <amiref007@gmail.com>
2026-05-27 19:30:10 +0330

    style: move tailwind CSS styling to component Input.tsx

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -10,7 +10,12 @@ interface Props {
 export default function Input({ onChange, placeholder, style, value }: Props) {
     return (
         <input
-            className={twMerge("input", style)}
+            className={twMerge(`
+                w-full h-10.5 px-3 border-default rounded-md bg-bg-primary
+                text-text-primary outline-none input-transition
+                placeholder:text-text-muted placeholder:text-xsm placeholder:font-extralight
+                focus:border-accent-primary focus:border-2
+            `, style)}
             placeholder={placeholder}
             value={value}
             onChange={onChange}
