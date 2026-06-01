7365d39a9542bd51d1c82a98459c98dd35f69221
Amir Ekbatanifard <amiref007@gmail.com>
2026-06-02 00:13:24 +0330

    enhance: add Icon to Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,14 +1,16 @@
+import type { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface Props {
-    title: string;
+    title?: string;
     onClick: () => void;
     type: "primary" | "secondary";
     style?: string;
+    Icon?: ReactNode;
 }
 
 const base =
-    "cursor-pointer inline-flex items-center justify-center gap-2 h-10 px-4 rounded-md text-text-primary button-transition";
+    "cursor-pointer inline-flex items-center justify-center gap-2 h-10 px-2.5 rounded-md text-text-primary button-transition";
 
 const variants = {
     primary:
@@ -17,13 +19,14 @@ const variants = {
         "border border-border-primary bg-bg-secondary text-sm font-medium hover:bg-bg-hover hover:border-border-secondary"
 };
 
-export default function Button({ title, onClick, type, style }: Props) {
+export default function Button({ Icon, title, onClick, type, style }: Props) {
     return (
         <button
             className={twMerge(`${base} ${variants[type]}`, style)}
             onClick={onClick}
         >
             {title}
+            {Icon}
         </button>
     );
 }
