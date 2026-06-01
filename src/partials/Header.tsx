082d2f5af08152a944d6f023d83c71a471bb0b97
Amir Ekbatanifard <amiref007@gmail.com>
2026-06-01 16:27:42 +0330

    feat: use ChainSwitch.tsx in Header partial

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -1,12 +1,11 @@
 import Brand from "./Brand";
+import ChainSwitch from "../components/ChainSwitch";
 
 export default function Header() {
     return (
         <div className="flex items-center justify-between p-4.5 border-b border-border-primary bg-bg-quaternary backdrop-blur-[10px] shadow-md">
             <Brand />
-            <div>
-                PAH
-            </div>
+            <ChainSwitch />
         </div>
     );
 }
