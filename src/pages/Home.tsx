4dc1d63829e79f961191c78caf50ca1d069617f4
Amir Ekbatanifard <amiref007@gmail.com>
2026-06-05 16:18:59 +0330

    refactor: separate Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import PopUp from "@/components/PopUp";
+import Utilities from "@/components/Utilities";
+import WatchContract from "@/components/WatchContract";
+import Content from "@/partials/Content";
+import Footer from "@/partials/Footer";
+import Header from "@/partials/Header";
+import { PopUps } from "@/utils/constants";
+import { useState } from "react";
+
+export default function Home() {
+    const [popup, setPopup] = useState<PopUps>(PopUps.None);
+
+    return (
+        <div className="page-container">
+            <Header />
+            <Content>
+                <WatchContract setPopup={setPopup} />
+                <Utilities setPopup={setPopup} />
+            </Content>
+            <Footer />
+            <PopUp popup={popup} setPopup={setPopup} />
+        </div>
+    );
+}
