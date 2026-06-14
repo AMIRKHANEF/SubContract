89db4168b4a6d7360bf3d5af8d1da390f5d1f08b
Amir Ekbatanifard <amiref007@gmail.com>
2026-06-14 11:40:05 +0330

    feat: create new ABI partial component SignatureHeader.tsx

---
+++ src/partials/abi/SignatureHeader.tsx
@@ -0,0 +1,30 @@
+import ScrollingTextBox from "@/components/ui/ScrollingTextBox";
+import Badge from "./Badge";
+import type { StateType } from "@/utils/ABI/ABIParser";
+
+interface SignatureHeaderProps {
+    state: StateType;
+    shortSignature: string;
+    name: string;
+}
+
+export default function SignatureHeader({ name, shortSignature, state }: SignatureHeaderProps) {
+    return (
+        <div className="flex flex-row items-center gap-2">
+            <Badge state={state} />
+            <ScrollingTextBox
+                text={name}
+                width={100}
+                textClassName="font-medium text-sm font-['JetBrains_Mono', monospace]"
+                scrollOnHover
+                preserveWidth
+            />
+            <ScrollingTextBox
+                text={shortSignature}
+                width={170}
+                textClassName="font-light text-xs text-text-muted"
+                scrollOnHover
+            />
+        </div>
+    );
+}
