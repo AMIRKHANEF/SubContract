ea1a4b66858a7e4bbee6e18f8ef75d225dc9b98a
Amir Ekbatanifard <amiref007@gmail.com>
2026-06-14 11:44:01 +0330

    feat: create new ABI partial component SignatureItem.tsx

---
+++ src/partials/abi/SignatureItem.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import Collapse from "@/components/ui/Collapse";
+import type { ParsedError, ParsedEvent, ParsedFunction, ParsedSpecialItem } from "@/utils/ABI/ABIParser";
+import { type ReactNode } from "react";
+import SignatureHeader from "./SignatureHeader";
+
+export default function SignatureItem({ item }: { item: ParsedFunction }): ReactNode
+export default function SignatureItem({ item }: { item: ParsedEvent }): ReactNode
+export default function SignatureItem({ item }: { item: ParsedError }): ReactNode
+export default function SignatureItem({ item }: { item: ParsedSpecialItem }): ReactNode
+export default function SignatureItem({ item }: { item: any }): ReactNode {
+    return (
+        <Collapse
+            collapseChildren={
+                <SignatureHeader
+                    name={item.name}
+                    shortSignature={item.shortSignature}
+                    state={item.stateMutability ?? item.kind}
+                />}
+            withChevron
+            className="p-2.5"
+        >
+            <></>
+        </Collapse>
+    );
+}
