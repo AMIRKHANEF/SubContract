279200fc8eea2325697f98ae41a279146a5316f8
Amir Ekbatanifard <amiref007@gmail.com>
2026-06-19 10:34:04 +0330

    refactor: separate and use ContractRelayer component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
+import ContractRelayer from './relayers/ContractRelayers.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
-)
+export default function Main() {
+  return (
+    <StrictMode>
+      <Provider store={store}>
+        <ContractRelayer />
+        <App />
+      </Provider>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<Main />);
